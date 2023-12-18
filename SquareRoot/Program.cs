f2883380d3cf93c90283a22a7461d1fcf98bc97e
António Caldeira <antoniocaldeira@yahoo.com>
2023-12-18 20:13:02 +0000

    Implemented the Square Root algo

---
+++ SquareRoot/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Please insert a positive number");
+string userInput = Console.ReadLine();
+
+int validatedNumber = ValidateInput(userInput);
+if (validatedNumber == -1)
+{
+    Console.WriteLine("Invalid input. Please enter a positive integer.");
+    return;
+}
+
+double result = CalculateSquareRoot(validatedNumber);
+Console.WriteLine($"The square root of {validatedNumber} is {result}");
+    
+
+    static int ValidateInput(string input)
+{
+    if (int.TryParse(input, out int parsedInt) && parsedInt >= 0)
+    {
+        return parsedInt;
+    }
+    return -1;
+}
+
+static double CalculateSquareRoot(int number)
+{
+    return Math.Sqrt(number);
+}
